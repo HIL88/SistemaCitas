5b51a631f4c86edb290e38591d9e0e4aeb02d952
Hilmer <hilserrano1520@gmail.com>
2021-12-18 16:17:55 -0600

    Add project files.

---
+++ Sistema/Datos/contextoBD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sistema.Models;
+
+namespace Sistema.Datos
+{
+    public class contextoBD:DbContext
+    {
+        public contextoBD(DbContextOptions<contextoBD> options) : base(options)
+        {
+
+        }
+        public DbSet<Cita> citas { get; set; }
+    }
+}
